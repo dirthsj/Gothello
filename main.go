1294122b312fc4b691e51c0d88b4b0a9d8256950
Steven Dirth <dirthsj@iastate.edu>
2020-04-05 11:04:05 -0500

    Add dynamic port binding

--- main.go
+++ main.go
@@ -1,33 +1,41 @@
 package main
 
 import (
+	"gothello/HttpHandlers"
+	"gothello/WebsocketHandlers"
 	"log"
 	"net/http"
+	"os"
 
 	_ "github.com/heroku/x/hmetrics/onload"
 )
 
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
 	handlerMap := make(map[string]http.Handler)
 	fs := http.FileServer(http.Dir("./www"))
 	handlerMap["/"] = fs
-	//handlerMap["/time"] = &HttpHandlers.TimeHandler{}
-	//handlerMap["/playerMove"] = &HttpHandlers.MovePlayerHandler{}
-	//handlerMap["/register"] = &HttpHandlers.PlayerRegistrationHandler{}
-	//handlerMap["/playerList"] = &HttpHandlers.PlayerListHandler{}
-	//for key, element := range handlerMap {
-	//	http.Handle(key, element)
-	//}
-	//chatHub := WebsocketHandlers.NewChatHub()
-	//go chatHub.Run()
-	//http.HandleFunc("/ws/chat", func(w http.ResponseWriter, r *http.Request) {
-	//	WebsocketHandlers.ServeChatWs(chatHub, w, r)
-	//})
-	//gameHub := WebsocketHandlers.NewGameHub()
-	//go gameHub.Run()
-	//http.HandleFunc("/ws/game/", func(w http.ResponseWriter, r *http.Request) {
-	//	WebsocketHandlers.ServeGameWs(gameHub, w, r)
-	//})
-	log.Fatal(http.ListenAndServe(":80", nil))
+	handlerMap["/time"] = &HttpHandlers.TimeHandler{}
+	handlerMap["/playerMove"] = &HttpHandlers.MovePlayerHandler{}
+	handlerMap["/register"] = &HttpHandlers.PlayerRegistrationHandler{}
+	handlerMap["/playerList"] = &HttpHandlers.PlayerListHandler{}
+	for key, element := range handlerMap {
+		http.Handle(key, element)
+	}
+	chatHub := WebsocketHandlers.NewChatHub()
+	go chatHub.Run()
+	http.HandleFunc("/ws/chat", func(w http.ResponseWriter, r *http.Request) {
+		WebsocketHandlers.ServeChatWs(chatHub, w, r)
+	})
+	gameHub := WebsocketHandlers.NewGameHub()
+	go gameHub.Run()
+	http.HandleFunc("/ws/game/", func(w http.ResponseWriter, r *http.Request) {
+		WebsocketHandlers.ServeGameWs(gameHub, w, r)
+	})
+	log.Fatal(http.ListenAndServe(":" + port, nil))
 }
