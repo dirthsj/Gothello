b4b2ed7e5c3cca7787f3ff7b02e82ad0a14f585a
Steven Dirth <dirthsj@iastate.edu>
2020-04-05 11:00:32 -0500

    testing

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"gothello/HttpHandlers"
-	"gothello/WebsocketHandlers"
 	"log"
 	"net/http"
 
@@ -14,22 +12,22 @@ func main() {
 	handlerMap := make(map[string]http.Handler)
 	fs := http.FileServer(http.Dir("./www"))
 	handlerMap["/"] = fs
-	handlerMap["/time"] = &HttpHandlers.TimeHandler{}
-	handlerMap["/playerMove"] = &HttpHandlers.MovePlayerHandler{}
-	handlerMap["/register"] = &HttpHandlers.PlayerRegistrationHandler{}
-	handlerMap["/playerList"] = &HttpHandlers.PlayerListHandler{}
-	for key, element := range handlerMap {
-		http.Handle(key, element)
-	}
-	chatHub := WebsocketHandlers.NewChatHub()
-	go chatHub.Run()
-	http.HandleFunc("/ws/chat", func(w http.ResponseWriter, r *http.Request) {
-		WebsocketHandlers.ServeChatWs(chatHub, w, r)
-	})
-	gameHub := WebsocketHandlers.NewGameHub()
-	go gameHub.Run()
-	http.HandleFunc("/ws/game/", func(w http.ResponseWriter, r *http.Request) {
-		WebsocketHandlers.ServeGameWs(gameHub, w, r)
-	})
+	//handlerMap["/time"] = &HttpHandlers.TimeHandler{}
+	//handlerMap["/playerMove"] = &HttpHandlers.MovePlayerHandler{}
+	//handlerMap["/register"] = &HttpHandlers.PlayerRegistrationHandler{}
+	//handlerMap["/playerList"] = &HttpHandlers.PlayerListHandler{}
+	//for key, element := range handlerMap {
+	//	http.Handle(key, element)
+	//}
+	//chatHub := WebsocketHandlers.NewChatHub()
+	//go chatHub.Run()
+	//http.HandleFunc("/ws/chat", func(w http.ResponseWriter, r *http.Request) {
+	//	WebsocketHandlers.ServeChatWs(chatHub, w, r)
+	//})
+	//gameHub := WebsocketHandlers.NewGameHub()
+	//go gameHub.Run()
+	//http.HandleFunc("/ws/game/", func(w http.ResponseWriter, r *http.Request) {
+	//	WebsocketHandlers.ServeGameWs(gameHub, w, r)
+	//})
 	log.Fatal(http.ListenAndServe(":80", nil))
 }
